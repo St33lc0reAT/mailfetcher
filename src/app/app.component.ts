0d23e500e98e4cde1ed90d8fc444f27cd1b773e0
Thomas <KeuschnigThomas@gmail.com>
2023-03-07 20:47:16 +0100

    Updated website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,110 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { stringify } from 'querystring';
+import { environment } from "src/environments/environment";
+
+
+export interface ListEntry {
+  name: string;
+  checked: boolean;
+  display: string;
+}
+
+export interface User {
+  vorname: string;
+  nachname: string;
+  mail: string;
+  tel: string;
+  birth: string;
+  lists: Array<string>;
+}
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
-  title = 'mailfetcher';
+  constructor(private http:HttpClient){}
+
+  title = 'Newsletter Anmeldung Kuchler-Fischer';
+  name = "test";
+  lastname = "";
+  mail = "";
+  tel = "";
+  birth = new Date();
+  agb = false;
+  newsList: ListEntry[] = [
+    {
+      name: "allgemein",
+      checked: false,
+      display: "Allgemeine Infos (Veranstaltungen, Versammlungen etc.)"
+    },
+    {
+      name: "balcke",
+      checked: false,
+      display: "Balcketeich"
+    },
+    {
+      name: "buergerau",
+      checked: false,
+      display: "Bürgerausee"
+    },
+    {
+      name: "wiestal",
+      checked: false,
+      display: "Wiestalstausee"
+    }
+  ];
+  valueChange($event:any) {
+    //set the two-way binding here for the specific unit with the event
+    this.agb = $event.checked;
+  }
+
+  submitUser()
+  {
+    /*console.log("Test");
+    let result = document.querySelector('.result');
+    let xhr = new XMLHttpRequest();
+    let url = "http://localhost:6444/adduser";
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4 && xhr.status === 200 && result)
+      {
+        console.log(this.responseText);
+      }
+    };*/
+    let data:User = {
+      "vorname": this.name,
+      "nachname": this.lastname,
+      "mail": this.mail,
+      "tel": this.tel,
+      "birth": this.birth.toLocaleString(),
+      "lists":  this.newsList.map((entry:ListEntry) => {
+          if(entry.checked === true){
+            return entry.name;
+          }
+          return "";
+        })
+    }
+    //xhr.send(data);
+    this.sendUser(data).subscribe(data => console.log(data));
+  }
+
+  sendUser(user: User):Observable<User> {
+    return this.http.post<User>(`${environment.apiUrl}/adduser`, user, httpOptions)
+    .pipe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import {MatInputModule } from '@angular/material/input'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgbModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
